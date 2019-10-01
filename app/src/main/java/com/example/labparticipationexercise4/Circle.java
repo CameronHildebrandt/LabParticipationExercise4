9d91b9065b098ab6d5d8ecd8449ee9025c59691d
cameronhildebrandt <cameronhildebrandt1@gmail.com>
2019-09-30 19:48:59 -0600

    changed colour to amaranth

--- app/src/main/java/com/example/labparticipationexercise4/Circle.java
+++ app/src/main/java/com/example/labparticipationexercise4/Circle.java
@@ -1,5 +1,5 @@
 package com.example.labparticipationexercise4;
 
 public class Circle extends Shape {
-    
+
 }
--- app/src/main/java/com/example/labparticipationexercise4/Shape.java
+++ app/src/main/java/com/example/labparticipationexercise4/Shape.java
@@ -3,5 +3,5 @@ package com.example.labparticipationexercise4;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "black";
+    String color = "amaranth";
 }
